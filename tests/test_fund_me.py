a2d4c58706bd0b0a3d256189c1355017ab10388b
Sayrikey1 <kamalseriki49@gmail.com>
2022-01-08 01:32:33 +0100

    initial commit

---
+++ tests/test_fund_me.py
@@ -0,0 +1,29 @@
+from scripts.helpful_scripts import get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS
+from scripts.deploy import deploy_fund_me
+from brownie import network, accounts, exceptions
+import pytest
+
+
+def test_can_fund_and_withdraw():
+    account = get_account()
+    fund_me = deploy_fund_me()
+    # +100 just in case you need more money to test
+    entrance_fee = fund_me.getEntranceFee() + 100
+    tx = fund_me.fund({"from": account, "value": entrance_fee})
+    tx.wait(1)
+    assert fund_me.addressToAmountFunded(account.address) == entrance_fee
+    tx2 = fund_me.withdraw({"from": account})
+    tx2.wait(1)
+    assert fund_me.addressToAmountFunded(account.address) == 0
+
+
+def test_only_owner_can_withdraw():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("only for local testing")
+    fund_me = deploy_fund_me()
+    # Since it's only owner that can withdraw, we create a random account to
+    # try withdraw. This raises an exception, but it works because that is what
+    # we want so we imported exceptions and make the test pass!!!
+    bad_actor = accounts.add()
+    with pytest.raises(exceptions.VirtualMachineError):
+        fund_me.withdraw({"from": bad_actor})
